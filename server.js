ee8050fba1081ded3424e31373506c860ca2919e
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-02-26 15:00:58 +0530

    add uuid

--- server.js
+++ server.js
@@ -2,5 +2,17 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+});
 
 server.listen(3000);
